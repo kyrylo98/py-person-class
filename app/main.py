3183f08ff2761a813479862ccbb30ca93bc5c3c3
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-04 20:26:08 +0200

    Find a pair "husband", "wife" in dict

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 class Person:
-
     people = {}
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None
+        self.husband = None
         Person.people[self.name] = self
 
-
 def create_person_list(people: list) -> list:
+    Person.people.clear()
     my_list = []
     for person_data in people:
         name = person_data["name"]
