5d1ca86d9fc486059aad77c81295deadde312569
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-04 19:49:33 +0200

    Find a pair "husband", "wife" in dict

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name:str, age:int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    my_list = []
+    for married in people:
+        name =married["name"]
+        age = married["age"]
+        person_instance = Person(name, age)
+        my_list.append(person_instance)
+    for married in people:
+        person_current = Person.people[married["name"]]
+        if "wife" in married and married["wife"]:
+            wife_name = married["wife"]
+            if wife_name in Person.people:
+                person_current.wife = Person.people[wife_name]
+        if "husband" in married and married["husband"]:
+            husband_name = married["husband"]
+            if husband_name in Person.people:
+                person_current.husband = Person.people[husband_name]
+    return my_list
+
+
+
+
