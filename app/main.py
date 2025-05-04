34d4b8462241943e364a95e0d6f2c1d6c63328c7
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-04 20:14:35 +0200

    Find a pair "husband", "wife" in dict

--- app/main.py
+++ app/main.py
@@ -1,30 +1,32 @@
 class Person:
     people = {}
-    def __init__(self, name:str, age:int) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
 
-
 def create_person_list(people: list) -> list:
     my_list = []
-    for married in people:
-        name =married["name"]
-        age = married["age"]
+    for person_data in people:
+        name = person_data["name"]
+        age = person_data["age"]
         person_instance = Person(name, age)
         my_list.append(person_instance)
-    for married in people:
-        person_current = Person.people[married["name"]]
-        if "wife" in married and married["wife"] is not None:
-            wife_name = married["wife"]
-            if wife_name in Person.people:
-                person_current.wife = Person.people[wife_name]
-        if "husband" in married and married["husband"] is not None:
-            husband_name = married["husband"]
-            if husband_name in Person.people:
-                person_current.husband = Person.people[husband_name]
-    return my_list
 
+    for person_data in people:
+        current_person = Person.people[person_data["name"]]
+
+        if "wife" in person_data and person_data["wife"] is not None:
+            wife_name = person_data["wife"]
+            if wife_name in Person.people:
+                current_person.wife = Person.people[wife_name]
+
+        if "husband" in person_data and person_data["husband"] is not None:
+            husband_name = person_data["husband"]
+            if husband_name in Person.people:
+                current_person.husband = Person.people[husband_name]
+
+    return my_list
 
 
 
